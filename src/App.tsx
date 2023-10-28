853955e6d73690a40f55841e4890020eb3d340d1
Haya Irfan <infinitelyrandomhaya@gmail.com>
2023-10-28 14:19:42 -0700

    Bootstrapped react application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Best Day 2 Marry - Haya Irfan Poc - Coming Soon... </h2>
+    </div>
+  );
+}
+
+export default App;
